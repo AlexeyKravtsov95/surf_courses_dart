88cf132fb89f8dd36b8ed945599aac71e5b57f67
aleksey.kravtsov <aleksey.kravtsov@vk.team>
2024-04-07 01:12:58 +0700

    Resources

---
+++ flutters_project/lib/task11/resources/style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorsData {
+  static const text = Color.fromRGBO(37, 40, 73, 1);
+  static const secondaryText = Color.fromRGBO(96, 96, 123, 1);
+  static const arrowColor = Color.fromRGBO(103, 205, 0, 1);
+  static const selectedItemColor = Color.fromRGBO(103, 205, 0, 1);
+  static const iconsBackgroundColor = Color.fromRGBO(241, 241, 241, 1);
+  static const iconsColor = Color.fromRGBO(96, 96, 123, 1);
+  static const lightGreen = Color.fromRGBO(103, 205, 0, 1);
+}
+
