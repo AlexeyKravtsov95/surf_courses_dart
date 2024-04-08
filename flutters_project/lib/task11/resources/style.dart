f3b9664bee1736b48f19e9207e38a24d0a78488e
aleksey.kravtsov <aleksey.kravtsov@vk.team>
2024-04-08 13:55:40 +0700

    Update style

--- flutters_project/lib/task11/resources/style.dart
+++ flutters_project/lib/task11/resources/style.dart
@@ -8,5 +8,7 @@ abstract class ColorsData {
   static const iconsBackgroundColor = Color.fromRGBO(241, 241, 241, 1);
   static const iconsColor = Color.fromRGBO(96, 96, 123, 1);
   static const lightGreen = Color.fromRGBO(103, 205, 0, 1);
+  static const lightGrey = Color.fromRGBO(181, 181, 181, 1);
+  static const errorColor = Color.fromRGBO(255, 0, 0, 1);
 }
 
